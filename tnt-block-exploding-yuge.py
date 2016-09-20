42dd85b92d23a80aaca6addc8f895a8ef714c270
impulselabsinc <nikhil@impulselabs.io>
2016-09-20 12:56:36 -0400

    exploding tnt

--- tnt-block-exploding-yuge.py
+++ tnt-block-exploding-yuge.py
@@ -3,7 +3,7 @@ import mcpi.minecraft as minecraft
 import mcpi.block as block
 
 mc = minecraft.Minecraft.create()
-tnt = block.TNT
+tnt = block.TNT_EXPLODING
 pos = mc.player.getTilePos()
 x = pos.x + 1
 y = pos.y
