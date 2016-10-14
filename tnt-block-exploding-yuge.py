d26fc47edb29c0100125b316001521685f3426e4
nikmeiser <nikmeiser@NIKHILs-MacBook-Pro.local>
2016-10-14 13:43:19 -0400

    updated tnt-yuge to be generic block

--- tnt-block-exploding-yuge.py
+++ tnt-block-exploding-yuge.py
@@ -3,10 +3,10 @@ import mcpi.minecraft as minecraft
 import mcpi.block as block
 
 mc = minecraft.Minecraft.create()
-tnt = block.TNT_EXPLODING
+myblock = block.GOLD_ORE
 pos = mc.player.getTilePos()
 x = pos.x + 1
 y = pos.y
 z = pos.z + 1
 
-mc.setBlocks(x, y, z, x+11, y+11, z+11, tnt)
+mc.setBlocks(x, y, z, x+11, y+11, z+11, myblock)
